36322712ed127d045abc17cb8f6127c197b06a35
BWolfgang01 <tun39109@temple.edu>
2025-10-16 19:17:53 -0400

    Added a bottom text to accompany the title

--- bioguessr-client/src/App.jsx
+++ bioguessr-client/src/App.jsx
@@ -3,19 +3,24 @@ import './App.css'
 
 function App() {
     const [title, setTitle] = useState('');
-
+    const [bottomTest, setBottomText] = useState('');
     useEffect(() => {
         const getTitle = async () => {
             const res = await fetch('/api/title');
             return await res.text();
         }
-
+        const getBottomText = async () => {
+            const res = await fetch('/api/bottomText');
+            return await res.text();
+        }
         setTitle(getTitle());
+        setBottomText(getBottomText());
     }, [])
 
     return (
         <>
             <h1>{title}</h1>
+            <p>{bottomTest}</p>
         </>
     )
 }
