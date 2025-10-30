d026266d8aed25cb7ac2fdc1191b132e46132db8
newgan <laskocsgo@gmail.com>
2025-10-30 10:29:49 -0400

    server and local db setup

--- bioguessr-server/server.js
+++ bioguessr-server/server.js
@@ -1,9 +1,26 @@
 import express from "express";
+const { DynamoDBClient, ScanCommand } = require("@aws-sdk/client-dynamodb");
 const app = express();
 const port = 3000;
 
-app.get("/api/title", (req, res) => {
-  res.send("BioGuessr");
+const client = new DynamoDBClient({
+  endpoint: "http://localhost:8000",
+  region: "us-east-1",
+  credentials: { accessKeyId: "fake", secretAccessKey: "fake" },
+});
+
+async function getRandomAnimal() {
+  const result = await client.send(new ScanCommand({ TableName: "animals" }));
+  const items = result.Items;
+
+  if (!items || items.length === 0) return null;
+
+  const randomIndex = Math.floor(Math.random() * items.length);
+  return items[randomIndex];
+}
+
+app.get("/api/play", async (req, res) => {
+  res.send(await getRandomAnimal());
 });
 
 app.get("/api/bottomText", (req, res) => {
