446a3a1de7e955e832d2b301d5a53ac07a62e625
BWolfgang01 <tun39109@temple.edu>
2025-12-02 19:56:21 -0500

    Made changes to config and packages

--- bioguessr-client/vite.config.js
+++ bioguessr-client/vite.config.js
@@ -1,16 +1,28 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-        secure: false, // change this for prod url
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '.', '');
+
+  return {
+    plugins: [react()],
+    server: {
+      cors: {
+        origin: env.AWS_URL,
+      },
+      proxy: {
+        '/api': {
+          target: env.AWS_URL || 'http://localhost:3000',
+          changeOrigin: true,
+        },
       },
     },
-  },
+    build: {
+      manifest: true,
+      rollupOptions: {
+        input: 'src/main.jsx',
+      },
+    },
+  };
 });
