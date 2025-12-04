86a3f795693fdff563ffd673b5c19cd275304614
newgan <laskocsgo@gmail.com>
2025-12-04 00:40:23 -0500

    clientside aws stuff

--- bioguessr-client/vite.config.js
+++ bioguessr-client/vite.config.js
@@ -1,28 +1,17 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
 
-// https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '');
+dotenv.config();
 
-  return {
-    plugins: [react()],
-    server: {
-      cors: {
-        origin: env.AWS_URL,
-      },
-      proxy: {
-        '/api': {
-          target: env.AWS_URL || 'http://localhost:3000',
-          changeOrigin: true,
-        },
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: process.env.AWS_URL,
+        changeOrigin: true,
       },
     },
-    build: {
-      manifest: true,
-      rollupOptions: {
-        input: 'src/main.jsx',
-      },
-    },
-  };
+  },
 });
